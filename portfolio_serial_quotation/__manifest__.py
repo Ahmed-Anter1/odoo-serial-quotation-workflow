e275967e2c54a3a071df8d659cf7f5f068f07f7d
ahmed anter <anterahmed818@gmail.com>
2026-07-17 06:02:57 +0300

    Add portfolio_serial_quotation/__manifest__.py

---
+++ portfolio_serial_quotation/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name": "Portfolio Serial Numbers on Quotations",
+    "version": "18.0.1.0.0",
+    "category": "Sales",
+    "summary": "Select serial numbers on quotation lines and transfer them to deliveries",
+    "license": "LGPL-3",
+    "depends": ["sale_stock"],
+    "data": ["views/sale_order_views.xml"],
+    "installable": True,
+}
